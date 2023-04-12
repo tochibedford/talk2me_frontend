446f869a68fa17feb4e3f4082fe1b4212742202f
tochibedford <musicofbyte@gmail.com>
2023-04-12 21:20:40 +0100

    feat: styled the search

--- src/components/SearchAndChat/Search/index.tsx
+++ src/components/SearchAndChat/Search/index.tsx
@@ -4,8 +4,17 @@ export default function Search() {
     const location = useLocation()
 
     return (
-        <div className={`${styles.container} ${location.pathname === "/" ? styles.focused : ""}`}>
-            <Link to="/tochibedford/chat">Search</Link>
-        </div>
+        <section role="search" className={`${styles.container} ${location.pathname === "/" ? styles.focused : ""}`}>
+            <form action="" method="get">
+                <fieldset>
+                    <legend>PUT IN A PUBLIC TWITTER HANDLE</legend>
+                    <label htmlFor="search">
+                        <span className={styles.atSymbol}>@</span>
+                        <input type="search" name="search" id="searchInput" />
+                        <button type="submit" title="Chat with this user now">CHAT</button>
+                    </label>
+                </fieldset>
+            </form>
+        </section>
     )
 }
